deb8933c1486b6beb4d2324461793c67b6877dd4
BianTan <1012874561@qq.com>
2018-04-10 20:47:24 +0800

    V1.0

--- app/src/main/java/org/biantan/subhd/AboutActivity.java
+++ app/src/main/java/org/biantan/subhd/AboutActivity.java
@@ -1,10 +1,16 @@
 package org.biantan.subhd;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class AboutActivity extends AppCompatActivity {
+import de.psdev.licensesdialog.LicensesDialog;
+
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,40 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Binding();
     }
+
+    private void Binding(){
+        LinearLayout developersView = (LinearLayout) findViewById(R.id.blog);
+        developersView.setOnClickListener(this);
+        LinearLayout licensesView = (LinearLayout) findViewById(R.id.ku);
+        licensesView.setOnClickListener(this);
+        LinearLayout sourceCodeView = (LinearLayout) findViewById(R.id.github);
+        sourceCodeView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.blog:
+                Intent intent1 = new Intent(Intent.ACTION_VIEW);
+                intent1.setData(Uri.parse("https://biantan.org"));
+                startActivity(intent1);
+                break;
+            case R.id.ku:
+                new LicensesDialog.Builder(this)
+                        .setNotices(R.raw.raw)
+                        .build()
+                        .show();
+                break;
+            case R.id.github:
+                Intent intent2 = new Intent(Intent.ACTION_VIEW);
+                intent2.setData(Uri.parse("https://github.com/BianTan/SubHD"));
+                startActivity(intent2);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
